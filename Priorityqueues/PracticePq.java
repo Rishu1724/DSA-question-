a10c4c09bdadfbff689dd0a891b764b7d6272e6f
Rishu kumar <rishuraj1721zo@gmail.com>
2026-01-26 12:09:19 +0530

    this is the new file

--- Priorityqueues/PracticePq.java
+++ Priorityqueues/PracticePq.java
@@ -1,6 +1,18 @@
-package Priorityqueues;
+// package Priorityqueues;
 import java.util.PriorityQueue;
 
 public class PracticePq {
-    
+    public static void main(String args[]){
+        PriorityQueue<Integer> pd = new PriorityQueue<>();
+        pd.add(2);
+        pd.add(7);
+        pd.add(4);
+        pd.add(5);
+        pd.add(3);
+        while(!pd.isEmpty()){
+            System.out.println(pd.peek());
+            pd.remove();
+        }
+
+    }
 }
