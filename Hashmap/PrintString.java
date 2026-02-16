b2ab6615e634a50ca65c9eb7d4f5480a931b5408
Rishu kumar <rishuraj1721zo@gmail.com>
2026-02-16 18:36:08 +0530

    new question

--- Hashmap/PrintString.java
+++ Hashmap/PrintString.java
@@ -1,13 +1,28 @@
-package Hashmap;
+// package Hashmap;
 
 import java.util.*;
+
+
+// public static class 
 
 public class PrintString {
     public static void main(String[] args) {
 
-        HashMap<Integer, String> hm = new HashMap<>();
-        hm.put(17, "mybday");
+        // HashMap<Integer, String> hm = new HashMap<>();
+        // hm.put(17, "mybday");
+        // hm.put(21,"hello");
+        // hm.put(23, " hello baby");hm.put(1,"ky hai hai");
+        // hm.remove(1);
 
-        System.out.println(hm);
+
+        // System.out.println(hm); 
+        HashMap<String,Integer> map= new HashMap<>();
+        map.put("rishu",1);
+        map.put("kumar",2);
+        map.put("kaise",3);
+        map.put("ho",4);
+        for(Map.Entry<String,Integer> e : map.entrySet()){
+            System.out.println("key:"+e.getKey()+"value"+e.getValue());
+        }
     }
 }
