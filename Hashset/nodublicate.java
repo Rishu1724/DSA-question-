5ce2439cfc17ed39890f825f20435d01b7e9048c
Rishu kumar <rishuraj1721zo@gmail.com>
2026-02-25 17:05:50 +0530

    new file

---
+++ Hashset/nodublicate.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class nodublicate {
+public static void main(String args[]){
+    HashSet<Integer> hs = new HashSet<>();
+    hs.add(2);
+    hs.add(3);
+    hs.add(4);
+    hs.add(4);
+    hs.add(9);
+    
+
+    System.out.println("this is the HashSet value" +hs);
+    // System.out.println(set.size());
+    // i am asseccing the Hashset value by the enhence for loop
+    for(Integer hi: hs){
+        System.out.println(hi);
+    }
+
+    }
+
+}
---
+++ Heap/Insert.java
@@ -0,0 +1,12 @@
+
+import java.util.PriorityQueue;
+
+public class Insert {
+    public static void main(String args[]){
+        PriorityQueue<String> pq = new PriorityQueue<>();
+        pq.add("rishu");
+        pq.add("kumar");
+        pq.add("iot");
+        System.out.println("String" +pq);
+    }
+}
