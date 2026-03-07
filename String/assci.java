d1c2680eca85ebe3fb02a6f5a9b5185cda8609ce
Rishu kumar <rishuraj1721zo@gmail.com>
2026-03-07 23:03:24 +0530

    this is the new solution

--- String/assci.java
+++ String/assci.java
@@ -5,6 +5,21 @@ public class assci {
 
         }
         // String str = new String ("Hello");
+        char ch = 'A';
+        int value= ch;
+        System.out.println(value);
+        System.out.println('a'-'A');
+
+
+        char ch1 = 'A';
+        if(ch1>='G' && ch1 <= 'Z'){
+            System.out.println("capital");
+        }
+
+
+
+
+
         String str = " hello";
         char[] rishu = str.toCharArray();
         for(int i=0; i<rishu.length;i++){
