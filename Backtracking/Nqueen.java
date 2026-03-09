b71c341470365eba7ec5d4b9789bbc6739c9e5fa
Rishu kumar <rishuraj1721zo@gmail.com>
2026-03-09 13:20:35 +0530

    this is the new question

--- Backtracking/Nqueen.java
+++ Backtracking/Nqueen.java
@@ -1,66 +1,70 @@
-// package Backtracking;
-
 public class Nqueen {
 
     public static void nQueens(char board[][], int row){
-        // base case;
+        // base case
         if(row == board.length){
             printBoard(board);
             return;
         }
-        //column loop;
-        for(int j=0; j<board.length;j++){
-            board[row][j] ='Q';
-            nQueens(board,row+1);// function call
-            board[row][j] ='x';// Backtracking step
+
+        // column loop
+        for(int j = 0; j < board.length; j++){
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';        // place queen
+                nQueens(board, row + 1);    // recursive call
+                board[row][j] = 'x';        // backtracking
+            }
         }
     }
-    public static boolean isSafe( char board[][], int row, int col){
-         // vertical up 
-        for(int i = row-1; i>=0;i--){
-            if(board[i][col]=='Q'){
+
+    public static boolean isSafe(char board[][], int row, int col){
+
+        // vertical up
+        for(int i = row - 1; i >= 0; i--){
+            if(board[i][col] == 'Q'){
                 return false;
             }
         }
-        // diag left up 
-        for(int i=row-1,j=col-1; i>=0 && j>=0; i--,j-- ){
-            if(board[i][j]=='Q'){
+
+        // diagonal left up
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+            if(board[i][j] == 'Q'){
                 return false;
             }
         }
-        // diag right up 
-        for(int i = row-1, j=col+1; i>=0 && j<board.length; i--,j++){
-            if(board[i][j]=='Q'){
+
+        // diagonal right up
+        for(int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++){
+            if(board[i][j] == 'Q'){
                 return false;
             }
         }
 
         return true;
-
     }
-       
-    
+
     public static void printBoard(char board[][]){
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
-                System.out.print(board[i][j] +" ");
+        System.out.println("----- Solution -----");
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
+
     public static void main(String args[]){
-        int n=4;
+        int n = 4;
         char board[][] = new char[n][n];
-        //initialize;
-        for(int i=0; i<n; i++){
-            for(int j=0;j<n; j++){
-                board[i][j] ='x';
+
+        // initialize board
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = 'x';
             }
         }
-        nQueens(board,0);
-        isSafe(board,4,4);
-        printBoard(board);
 
-
+        nQueens(board, 0);
     }
 }
