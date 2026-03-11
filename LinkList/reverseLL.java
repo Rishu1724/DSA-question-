d699c98df9a47eb5034b04048ff82bba6d09ed31
Rishu kumar <rishuraj1721zo@gmail.com>
2026-03-11 13:14:31 +0530

    update code

--- LinkList/reverseLL.java
+++ LinkList/reverseLL.java
@@ -1,6 +1,27 @@
-public class reverseLL {
-    Node{
-        
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
     }
-    
+}
+
+public class reverseLL {
+
+    public static void main(String args[]) {
+
+        Node head = new Node(10);
+        head.next = new Node(30);
+        head.next.next = new Node(40);
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
 }
