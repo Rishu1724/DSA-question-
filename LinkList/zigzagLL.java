ce9f3d99c518432e441a2c2728cb60f2e061a536
Rishu1724 <rishuraj1721zo@gmail.com>
2026-03-22 15:07:57 +0530

    new question

--- LinkList/zigzagLL.java
+++ LinkList/zigzagLL.java
@@ -3,5 +3,13 @@ public class zigzagLL {
 
     public static void main(String args[]){
         LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        System.out.println(ll);
+
+
     }
 }
