94c757f1798a9470a8bb527fca5be867ff518673
Rishu1724 <rishuraj1721zo@gmail.com>
2026-04-02 13:37:00 +0530

    new question

---
+++ Backtracking/ratinMaze.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class ratinMaze {
+    public static void main(String args[]){
+        
+    }
+}
---
+++ Recursion/combinationsum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class combinationsum {
+
+
+    public List<List<Integer>> comSum(int arr){
+        ArrayList<Integer> list = new ArrayList<>();
+        int n =arr.length;
+
+        for(int i =0;i <arr; i++)
+    }
+    public static void main(String args[]){
+        int arr[] = {2,3,5};
+    }
+}
