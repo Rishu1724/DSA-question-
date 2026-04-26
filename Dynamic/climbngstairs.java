1b19c0c97aa3427d5a89e02bf370abf35a13fe5f
Rishu1724 <rishuraj1721zo@gmail.com>
2026-04-26 23:51:31 +0530

    this is new file

---
+++ Dynamic/climbngstairs.java
@@ -0,0 +1,29 @@
+public class climbngstairs {
+    public static int stairs(int n){
+        int[] dp= new int[n+1];
+        return helper(n,dp);
+
+
+    }
+    public static int helper(int n , int dp){
+        if(n==0){
+            return 1;
+
+        }
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        return helper(n-1, dp)+ helper(n-2,dp);// this is the helper function 
+
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+    }
+}
+git add .
+git commit -m " this is new file"
+git push origin main
