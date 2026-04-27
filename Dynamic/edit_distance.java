e8dadc755506d301b077da2e06a9b18a85446138
Rishu1724 <rishuraj1721zo@gmail.com>
2026-04-27 23:17:33 +0530

    this is new file

--- Dynamic/edit_distance.java
+++ Dynamic/edit_distance.java
@@ -6,6 +6,7 @@ public class edit_distance {
         int dp[][] =new int[n+1][m+1];
 
         // initialize the dp 
+        // pahla line and dusra line
         for(int i=0 ; i<n+1; i++){
             for(int j=0;j<m+1; j++){
                 if(i==0){
