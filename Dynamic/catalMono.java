065a987d40e80d170d2e6b4f7e5b9763f2caf925
Rishu1724 <rishuraj1721zo@gmail.com>
2026-04-28 13:10:05 +0530

    this is new file

--- Dynamic/catalMono.java
+++ Dynamic/catalMono.java
@@ -1,6 +1,7 @@
 public class catalMono {
     public static int cat(int n ){
         int dp[]= new int[n+1];
+        int ans =0;
         // base case 
         if(n==0 || n==1){
             return 1;
@@ -9,7 +10,10 @@ public class catalMono {
             return dp[n];
         }
         // main recur + mono function 
-        for()
+        for(int i=0; i<n; i++){
+            ans+= cat(i)*cat(n-i-1);
+        }
+        return dp[n]=ans;
         
     }
     public static void main(String[] args) {
