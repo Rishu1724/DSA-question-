138275a5a3a6e108fb48dd0bd7189354ba20e6bd
Rishu1724 <rishuraj1721zo@gmail.com>
2026-05-14 10:36:54 +0530

    update

--- Dynamic/lcsub.java
+++ Dynamic/lcsub.java
@@ -7,6 +7,8 @@ public class lcsub {
         int ans = 0;
 
         // bottom up
+        // its a tabulation code without recursion
+        
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(strn1.charAt(i-1) == str2.charAt(j-1)){
