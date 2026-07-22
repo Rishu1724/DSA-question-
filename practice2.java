7b0f69e8430d086ebcb17187542f0fc7a303a7a1
Rishu1724 <rishuraj1721zo@gmail.com>
2026-07-22 21:42:12 +0530

    new code push

---
+++ practice2.java
@@ -0,0 +1,10 @@
+public class practice2 {
+    public static void main(String args[]) {
+        System.out.println("hello");
+        int a = 5;
+        int b=10;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+}
